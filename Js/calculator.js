6c6527e5b1bafec6af6d2b050cd44ac789af4d64
yunggenius <yunggenius4@gmail.com>
2019-09-25 11:16:21 +0100

    partially fixed calculator.html

--- Js/calculator.js
+++ Js/calculator.js
@@ -1,15 +1,32 @@
+			var id = 2;
+		function myFunction(){
+			var content = ''
+			content += '<tr class="tbody" id="'+ id +'"><td><input type="text" name="text" style="width: 180px;"></td>';							
+			content += '<td><input type="checkbox" style="width: 80px;"></td><td><input type="text" name="text" style="width: 60px;"></td>';
+			content += '<td><input type="text" name="text" style="width: 70px;"></td><td><input type="text" name="text" style="width: 70px;"></td>';
+			content += '<td><input type="text" name="text" style="width: 70px;"></td><td><input type="text" name="text" style="width: 70px;"></td>';
+			content += '<td><input type="text" name="text" style="width: 70px;"></td><td><button onclick="deleteRow('+ id +')">X</button></td></tr>';
+			document.getElementById('tbody').innerHTML += content;
+			id++;			
+		}
+
+		function deleteRow(id)
+		{
+			$('tr#' + id).remove();
+		}
+
 	function calculate() {
 		var myBox1 = document.getElementById('box1').value;	
 		var myBox2 = document.getElementById('box2').value;	
 		var myBox3 = document.getElementById('box3').value;
 		//for ssecond line
-		var myCat1 = document.getElementById('cat1').value;	
-		var myCat2 = document.getElementById('cat2').value;	
-		var myCat3 = document.getElementById('cat3').value;
+		// var myCat1 = document.getElementById('cat1').value;	
+		// var myCat2 = document.getElementById('cat2').value;	
+		// var myCat3 = document.getElementById('cat3').value;
 		//thrid line
-		var myDog1 = document.getElementById('dog1').value;	
-		var myDog2 = document.getElementById('dog2').value;	
-		var myDog3 = document.getElementById('dog3').value;
+		// var myDog1 = document.getElementById('dog1').value;	
+		// var myDog2 = document.getElementById('dog2').value;	
+		// var myDog3 = document.getElementById('dog3').value;
 		var result = document.getElementById('result');
 		var result2 = document.getElementById('result2');
 		var result3 = document.getElementById('result3');
@@ -18,13 +35,14 @@
 		var resultKW = document.getElementById('resultKW');	
 
 		var myResult = myBox1 * myBox2 * myBox3; //firts line
-		var myResult4 = myDog1 * myDog2 * myDog3;//thrid line
-		var myResult3 = myCat1 * myCat2 * myCat3;//second line
-		var myResult2 = myResult + myResult3 + myResult4; //sum total of watts
+		// var myResult4 = myDog1 * myDog2 * myDog3;//thrid line
+		// var myResult3 = myCat1 * myCat2 * myCat3;//second line
+		var myResult2 = myResult //+ myResult3 + myResult4; //sum total of watts
 		var kw = myResult2 / 1000;  //converts watts to kilowatts
 		result.value = myResult; //prints watts
 		resultKW.value = kw;	//prints Kilowatts @yinkaaaaaa
 		result2.value = myResult2;
-		result3.value = myResult3;
-		result4.value = myResult4;
+		// result3.value = myResult3;
+		// result4.value = myResult4;
 	}
+
